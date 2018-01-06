1c5d13ca11b635f6ac6ac8ab229ed5600e9637bd
Amine Fradi <amine.fradi@gmail.com>
2018-01-06 14:45:43 +0100

    change package name

--- spring-boot-web-spring-security/src/main/java/com/security/springsecurity/controller/DefaultController.java
+++ spring-boot-web-spring-security/src/main/java/com/security/springsecurity/controller/DefaultController.java
@@ -1,4 +1,4 @@
-package com.mkyong.springbootwebspringsecurity.controller;
+package com.security.springsecurity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
