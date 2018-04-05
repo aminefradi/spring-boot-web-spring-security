3c5e1260b6688a24bacd762ada5356128c6298c1
aminefradi <amine.fradi@gmail.com>
2018-04-05 17:36:47 +0100

    delete space

--- spring-boot-web-spring-security/src/main/java/com/security/springsecurity/config/SpringSecurityConfig.java
+++ spring-boot-web-spring-security/src/main/java/com/security/springsecurity/config/SpringSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 
-   @Configuration
+@Configuration
 
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
